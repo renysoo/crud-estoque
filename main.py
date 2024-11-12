1d6c9087e1fdbb8fda9d959a1131101cb53122c2
renysoo <rml2@cesar.school>
2024-11-12 17:50:42 -0300

    Add menu_funcionarios no main

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'crud-funcionarios'))
+
 from crud_vendas import menu_vendas 
 from crud_produtos import menu_produtos
-# from crud_funcionarios
+from painel_funcionarios import menu_funcionarios
 
 def menu():
     while True:
@@ -17,7 +22,7 @@ def menu():
         elif escolha == '2':
             menu_vendas()
         elif escolha == '3':
-            print('gerenciando produto')
+            menu_funcionarios()
         elif escolha == '4':
             print('*** Encerrando programa ***')
             break
